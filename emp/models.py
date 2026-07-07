25974200a91a15ed004a10caacc96ff4004120ea
harsukhbhuva <harsukh@avdevs.com>
2026-07-07 17:04:57 +0530

    Add employee model

--- emp/models.py
+++ emp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from account.models import User
 # Create your models here.
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class EmployeeProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=255)
+    address = models.TextField()   
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return self.user.username
